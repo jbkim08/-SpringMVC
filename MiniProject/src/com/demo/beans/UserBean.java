8ac4eacaf49d3ca92c069c7debe9b3bb6ff73410
jbkim08 <jbkim08@gmail.com>
2022-12-13 14:23:34 +0900

    회원가입

--- MiniProject/src/com/demo/beans/UserBean.java
+++ MiniProject/src/com/demo/beans/UserBean.java
@@ -1,11 +1,27 @@
 package com.demo.beans;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class UserBean {
 	
 	private int user_idx;
+	
+    @Size(min=2, max=4, message = "이름은 2자에서 4자까지")
+	@Pattern(regexp = "[가-힣]*", message = "한글만 사용가능")
 	private String user_name;
+    
+	@Size(min=4, max=10, message = "아이디는 4자에서 10자까지")
+	@Pattern(regexp = "[a-zA-Z0-9]*", message = "영문대소문자와 숫자만 사용가능")
 	private String user_id;
+	
+	@Size(min=4, max=20, message = "패스워드는는 4자에서 20자까지")
+	@Pattern(regexp = "[a-zA-Z0-9]*", message = "영문대소문자와 숫자만 사용가능")
 	private String user_pw;
+	
+	@Size(min=4, max=20, message = "패스워드는는 4자에서 20자까지")
+	@Pattern(regexp = "[a-zA-Z0-9]*", message = "영문대소문자와 숫자만 사용가능")
+	private String user_pw2;
 	
 	public int getUser_idx() {
 		return user_idx;
@@ -30,6 +46,12 @@ public class UserBean {
 	}
 	public void setUser_pw(String user_pw) {
 		this.user_pw = user_pw;
+	}
+	public String getUser_pw2() {
+		return user_pw2;
+	}
+	public void setUser_pw2(String user_pw2) {
+		this.user_pw2 = user_pw2;
 	}
 	
 	
